03324001e137c037a6c02c813d89b26153f4b051
ddpalacios <palaciosdanieldario@gmail.com>
2020-01-23 16:00:15 -0600

    Edited xml document and parsed within main to retrieve text within XML

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-import bs4
-
-
+from bs4 import BeautifulSoup
+from time import sleep
 html = open("xml_test.xml", 'r')
-soup = bs4.BeautifulSoup(html, 'lxml')
-print(soup)
+exam = BeautifulSoup(html, 'xml')
+
+all_questions_on_exam = exam.find_all('Question')
+all_answers_on_exam = exam.find_all('Answer')
+for question_num, (each_question, each_answer) in enumerate(zip(all_questions_on_exam, all_answers_on_exam)):
+    question_as_text = each_question.text
+    answer_as_text = each_answer.text
+    print("Question# {}\n{}\nANSWER: {}".format(question_num, question_as_text, answer_as_text))
+    print("----------------")
+    sleep(1)
+
+
+
