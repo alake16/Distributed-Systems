17987163ee5a67219f5e105acddd2c84480ef6e6
ddpalacios <palaciosdanieldario@gmail.com>
2020-02-08 16:59:23 -0500

    setting up tracking responses

--- app/server.py
+++ app/server.py
@@ -60,6 +60,18 @@ def deactivateQuestion():
         return responses
     return f'No Active Question!'
 
+'''
+Implement a method that allows us to track each incoming
+response for each answer
+
+'''
+@app.route('/trackResponse', methods=['POST'])
+def trackResponse():
+    global activeQuestion
+    if activeQuestion is not None:
+        pass
+
+
 
 @app.route('/recordResponse', methods=['POST'])
 def recordResponse():
