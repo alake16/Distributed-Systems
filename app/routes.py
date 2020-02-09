0f9e396293155d4f58e582df600fb28dfbc6b7a0
alake16 <aclake@umich.edu>
2020-02-09 16:53:46 -0600

    Routes and logic for creating a quiz with multiple choice questions

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,12 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, flash
 from app.forms import MultipleChoiceQuestionForm, NewQuizForm
+from app.Models.Quiz import Quiz
+from app.Models.Questions import MultipleChoiceQuestion
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'you-will-never-guess'
+
+createdQuiz = None
 
 # General Landing Page
 @app.route('/')
@@ -17,38 +21,61 @@ def admin():
 # ===== Quiz Creation Routes =====
 
 # Quiz Creator Page
-@app.route('/createQuiz')
+@app.route('/createQuiz', methods=['GET', 'POST'])
 def createQuiz():
+    global createdQuiz
     form = NewQuizForm()
     if form.validate_on_submit():
         flash("New Quiz Created")
-        return redirect(url_for("addQuestions"))
+        createdQuiz = Quiz(name=form.name.data)
+        return redirect(url_for("addQuestions", quizName=createdQuiz.name))
     return render_template("createQuiz.html", title="Create a Quiz", form=form)
 
 # Question Creator Landing Page
 @app.route('/addQuestions/<quizName>')
 def addQuestions(quizName):
     return render_template("addQuestions.html", title="Add Questions to " + quizName,
-                           quizName=quizName)
+                           quizName=quizName, quiz=createdQuiz)
 
 # Specific Question Type Creator
-@app.route('/addQuestions/<questionType>')
-def addQuestionsType(questionType):
-    if questionType == "multipleChoice":
-        form = MultipleChoiceQuestionForm()
-        if form.validate_on_submit():
-            flash("New Multiple Choice Question Added")
-            return redirect(url_for("addQuestions"))
-        return render_template("newMultipleChoiceQuestion.html", title="Add " + questionType +
-                               " Question")
-    else:
-        if questionType == "multipleChoice":
+@app.route('/addQuestion/<questionType>', methods=['GET', 'POST'])
+def addQuestion(questionType):
+    global createdQuiz
+    if createdQuiz is not None:
+        if questionType == "multiple_choice":
             form = MultipleChoiceQuestionForm()
             if form.validate_on_submit():
-                flash("New Matching Question Added")
-                return redirect(url_for("addQuestions"))
-        return render_template("newMatchingQuestion.html", title="Add " + questionType +
-                               " Question")
+                choices = {
+                    "A": form.choiceA.data,
+                    "B": form.choiceB.data,
+                    "C": form.choiceC.data,
+                    "D": form.choiceD.data,
+                    "E": form.choiceE.data,
+                }
+                # get rid of any empty choices
+                choices = {k: v for k, v in choices.items() if v}
+                newMultChoiceQuestion = MultipleChoiceQuestion(prompt=form.prompt.data,
+                                                               choices=choices,
+                                                               answer=form.answer.data)
+                createdQuiz.add_question_to_quiz(question=newMultChoiceQuestion)
+                print(createdQuiz)
+                return redirect(url_for("addQuestions", quizName=createdQuiz.name))
+            return render_template("newMultipleChoiceQuestion.html", title="Add " + questionType +
+                                   " Question", quizName=createdQuiz.name, form=form)
+        # else:
+        #     form = MatchingQuestionForm()
+        #     if form.validate_on_submit():
+        #         flash("New Matching Question Added")
+        #         return redirect(url_for("addQuestions"))
+        #     return render_template("newMatchingQuestion.html", title="Add " + questionType +
+        #                            " Question")
+
+# Quiz Session Creator Page
+@app.route('/writeQuiz', methods=['GET', 'POST'])
+def writeQuiz():
+    global createdQuiz
+    Quiz.write_quiz(createdQuiz, taken=False)
+    return redirect(url_for('admin'))
 
 # ===== Quiz Administration Routes =====
 
@@ -62,7 +89,7 @@ def activateQuiz():
 def taker():
     return render_template("taker.html", title="Take a Quiz")
 
-@app.errorhandler(404)
-def notfound():
-    """Serve 404 template."""
-    return make_response(render_template("404.html"), 404)
+# @app.errorhandler(404)
+# def notfound():
+#     """Serve 404 template."""
+#     return make_response(render_template("404.html"), 404)
