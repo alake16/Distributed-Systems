89ca972b7eb6201166cedad2a9f4dcf8d09e47d1
VismarkJuarez <vismarkjuarez@gmail.com>
2020-02-14 00:37:14 -0600

    Added route logic for preparing the counts.

--- app/routes.py
+++ app/routes.py
@@ -129,8 +129,65 @@ def takeQuiz():
 
 # ----------------Histograms------------------
 
-@app.route("/<int:bars_count>")
+
+def create_hover_tool():
+    # we'll code this function in a moment
+    return None
+
+
+def create_bar_chart(data, title, x_name, y_name, hover_tool=None,
+                     width=1200, height=300):
+    """Creates a bar chart plot with the exact styling for the centcom
+       dashboard. Pass in data as a dictionary, desired plot title,
+       name of x axis, y axis and the hover tool HTML.
+    """
+    source = ColumnDataSource(data)
+    xdr = FactorRange(factors=data[x_name])
+    ydr = Range1d(start=0,end=max(data[y_name])*1.5)
+
+    tools = []
+    if hover_tool:
+        tools = [hover_tool,]
+
+    plot = figure(title=title, x_range=xdr, y_range=ydr, plot_width=width,
+                  plot_height=height, h_symmetry=False, v_symmetry=False,
+                  min_border=0, toolbar_location="above", tools=tools,
+                  responsive=True, outline_line_color="#666666")
+
+    glyph = VBar(x=x_name, top=y_name, bottom=0, width=.8,
+                 fill_color="#e12127")
+    plot.add_glyph(source, glyph)
+
+    xaxis = LinearAxis()
+    yaxis = LinearAxis()
+
+    plot.add_layout(Grid(dimension=0, ticker=xaxis.ticker))
+    plot.add_layout(Grid(dimension=1, ticker=yaxis.ticker))
+    plot.toolbar.logo = None
+    plot.min_border_top = 0
+    plot.xgrid.grid_line_color = None
+    plot.ygrid.grid_line_color = "#999999"
+    plot.yaxis.axis_label = "Bugs found"
+    plot.ygrid.grid_line_alpha = 0.1
+    plot.xaxis.axis_label = "Days after app deployment"
+    plot.xaxis.major_label_orientation = 1
+    return plot
+
+@app.route("/<int:bars_count>/")
 def chart(bars_count):
-    if bars_count <=0:
-        bars_count=1
-    return render_template("chart.html", bars_count=bars_count)
+    if bars_count <= 0:
+        bars_count = 1
+
+    data = {"days": [], "bugs": [], "costs": []}
+    for i in range(1, bars_count + 1):
+        data['days'].append(i)
+        data['bugs'].append(random.randint(1,100))
+        data['costs'].append(random.uniform(1.00, 1000.00))
+
+    hover = create_hover_tool()
+    plot = create_bar_chart(data, "Bugs found per day", "days",
+                            "bugs", hover)
+    script, div = components(plot)
+
+    return render_template("chart.html", bars_count=bars_count,
+                           the_div=div, the_script=script)
