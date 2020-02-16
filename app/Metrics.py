3444028f1ac29434b9c17b76ab18b181130905dd
Daniel <palaciosdanieldario@gmail.com>
2020-02-16 16:18:36 -0600

    methods to retrieve given metrics by quiz name

--- app/Metrics.py
+++ app/Metrics.py
@@ -1,9 +1,10 @@
 class Metric:
 
     def __init__(self, quiz_name, question_list): # Or as single variable (dictionary)
-        self.quiz_name = quiz_name
-        self.question_list = question_list
 
+        ''' Initialize quiz name to open all data given from active question '''
+        self.quiz_name = quiz_name
+        self.question_list = question_list # Temp ?
 
 
     def retrieve_metrics(self):
@@ -15,22 +16,28 @@ class Metric:
         all_questions = self.question_list[self.quiz_name]
         all_responses = all_questions.get_responses()
 
-        for each_response in all_responses:
-            self.append_evaluated_response(each_response)
+        metrics = self.calculate_metrics_from(all_responses)
+        return metrics # String vesrion of percentage of selected question from each student
 
 
-        return 
-
-
+    def calculate_metrics_from(self, all_responses):
+        ''' Count responses given in list and calcualte
+        decision metrics based on the recieved responses '''
+        return
 
     def append_evaluated_response(self, response):
         '''
         From here we will be able to append each response
         to our current histogram as it is being iterated
         '''
-        return
+        student_metrics = self.caluclate_current_question_metric(resonse)
+        return student_metrics
+
 
     def getAllQuestionsAndResponses(self):
+
+        ''' Retrieve all data that was given in the dictionary by searching 
+        quiz by name to retrieve data '''
 
         all_questions = self.question_list[self.quiz_name]
         responses = all_questions.get_responses()
