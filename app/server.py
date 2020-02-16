33a3ad62c00dd67c7ae60056ef4b3fbf4608bbe2
Vismark Juarez <vismarkjuarez@gmailcom>
2020-02-16 05:12:56 -0600

    Refactored logic to accept different questions to different quizzes by adding a query argument (quizName

--- app/server.py
+++ app/server.py
@@ -9,13 +9,16 @@ from app.JSONHandler import ProjectJSONEncoder
 app = Flask(__name__)
 
 activeQuestion = None
+currentQuizName = None
 
 '''
 Will store all of the instructor's posed questions
 -- which, in turn, also provides a list of all the
 answer choices and answers.
 '''
-allQuestions = []
+allQuestionsForCurrentQuiz = []
+
+allQuestionsByQuizName = {}
 
 @app.route('/')
 def welcome():
@@ -26,6 +29,10 @@ def welcome():
 @app.route('/activateQuestion', methods=['POST', 'GET'])
 def activateQuestion():
     global activeQuestion
+    global currentQuizName
+    currentQuizName = request.args['quizName']
+    print('quizName should be printed below.')
+    print('quizName: {}'.format(currentQuizName))
 
     if request.method == 'POST':
         print('received the following payload: {}'.format(request.json))
@@ -58,13 +65,20 @@ def fetchResponses():
 def deactivateQuestion():
     global activeQuestion
 
-    '''
-    Store the question object in the list of allQuestions.
-    This is done now (when the question is being deactivated)
-    in order to aggregate the student responses and present
-    the question's metrics on the instructor UI.
-    '''
-    allQuestions.append(activeQuestion)
+    #If the quizName is found, simply add the currentQuestion to the quiz's list of Questions
+    if currentQuizName in allQuestionsByQuizName:
+        allQuestionsByQuizName[currentQuizName].append(activeQuestion)
+    else:
+        #create a new key entry in the dictionary with the currentQuizName
+        '''
+        Store the question object in the list of allQuestions.
+        This is done now (when the question is being deactivated)
+        in order to aggregate the student responses and present
+        the question's metrics on the instructor UI.
+        '''
+        allQuestionsForCurrentQuiz.append(activeQuestion)
+        allQuestionsByQuizName[currentQuizName] = allQuestionsForCurrentQuiz.copy()
+        allQuestionsForCurrentQuiz.clear()
 
     if activeQuestion is not None:
         responses = activeQuestion._responses
@@ -92,7 +106,7 @@ Returns all of the questions posed by the instructor.
 TODO: These should eventually be queried by sessionId and quizName.
 TODO: These should also eventually be stored in a database.
 '''
-@app.route('/allQuestion', methods=['GET'])
+@app.route('/allQuestionsByQuizName', methods=['GET'])
 def retrievCounts():
-    return flaskResponse(json.dumps(allQuestions, cls=ProjectJSONEncoder), 200,
+    return flaskResponse(json.dumps(allQuestionsByQuizName, cls=ProjectJSONEncoder), 200,
                              {'Content-Type': 'application/json'})
