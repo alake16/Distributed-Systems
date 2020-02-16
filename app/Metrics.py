61a77ee0f57655f1fb9d4fa970998eeaa1f39fc7
Daniel <palaciosdanieldario@gmail.com>
2020-02-16 15:40:33 -0600

    Added Metrics class fro visualization implementations. Currently adding methods to handle saved question and responses for later use

---
+++ app/Metrics.py
@@ -0,0 +1,14 @@
+class Metric:
+
+    def __init__(self, question, response): # Or as single variable (dictionary)
+        self.question = question
+        self.response = response
+
+
+
+        
+    def get_reponses(self):
+        pass
+
+    def getAllQuestionsAndResponses(self):
+        pass
